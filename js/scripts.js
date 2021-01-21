8d902f0412d3ee5a3d0bfd500f0f3cb7afa952bd
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-20 16:43:53 -0800

    working on second test
    Co-authored-by: Andrew Lawson <alawson89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 function pigLatin(user) {
   let vowels = ['e', 'u', 'i', 'o', 'a'];
+  let array = [];
   let sentence;
   if (/[euioa]/i.test(user[0])) {
     sentence = user + "way";
   } else {
-    sentence = "not here";
+    for (let i = 0; i < user.length; i++) {
+      if (/[^euioa]/i.test(user[i])) {
+        array.push(user[i])
+      } else {
+        let user1 = user.slice(i);
+        break;
+      }
+    }
+
+
+    //sentence = "not here";
   }
   // TODO
+
+
+
   return sentence;
 }
 
